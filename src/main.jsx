11d4062950f9c2724f116454ebeaf5a11c16a34c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-11 03:34:59 -0700

    Tarea - Componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FirstApp } from "./FirstApp";
+//import { FirstApp } from "./FirstApp";
 // import { HelloWorldApp } from "./HelloWorldApp";
 
+import { CounterApp } from "./CounterApp";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp title="Hola Mundo"/>
+    <CounterApp value={5} />
   </React.StrictMode>
 );
