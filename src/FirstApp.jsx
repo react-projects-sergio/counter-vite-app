084ba35af572571120b9f3b2a1a86c18d48885a8
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-11 03:24:33 -0700

    feat: DefaultProps

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,15 +1,21 @@
 import PropTypes from "prop-types";
 
-export const FirstApp = ({ title, subTitle }) => {
+export const FirstApp = ({
+  title,
+  subTitle = "No hay subtitulo",
+  name = "Sergio",
+}) => {
   return (
     <>
       <h1>{title}</h1>
       <p>{subTitle}</p>
+      <p>{name}</p>
     </>
   );
 };
 
 FirstApp.propTypes = {
   title: PropTypes.string.isRequired,
-  subTitle: PropTypes.number
+  subTitle: PropTypes.string,
+  name: PropTypes.string,
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp title="Hola, soy Sergio" />
+    <FirstApp title="Hola Mundo"/>
   </React.StrictMode>
 );
