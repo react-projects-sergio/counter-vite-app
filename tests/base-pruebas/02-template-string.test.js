6a557959747d27c20bc81ba42234c016ca711728
Sergio Barreras <sergiobg.isc@gmail.com>
2025-04-18 11:36:07 -0700

    Pruebas en el archivo 02-template-string.js

---
+++ tests/base-pruebas/02-template-string.test.js
@@ -0,0 +1,10 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
+
+describe("Pruebas en 02-template-string", () => {
+  test('getSaludo debe retornar "Hola Sergio"', () => {
+    const name = "Sergio";
+    const message = getSaludo(name);
+
+    expect(message).toBe(`Hola ${name}`);
+  });
+});
